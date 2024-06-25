02f4713435b87b66fe3eafb8506240a5e2ceb9dd
Matthew Michilot <matthew.michilot@gmail.com>
2024-06-24 18:35:28 -0700

    Have mypy ignore some types until they're stubbed

--- cocotb_kernel/module.py
+++ cocotb_kernel/module.py
@@ -1,7 +1,8 @@
-# type: ignore
+# TODO: Remove 'type: ignore' once cocotb v1.9 releases, which contains type stubs
+# TODO: Create type stubs for CocotbKernelApp
 import signal
-import cocotb
-from cocotb.handle import SimHandleBase
+import cocotb # type: ignore
+from cocotb.handle import SimHandleBase # type: ignore
 from typing import Any, Coroutine
 from ipykernel.kernelapp import IPKernelApp
 from IPython.core.interactiveshell import ExecutionResult
@@ -13,32 +14,32 @@ class CocotbKernelApp(IPKernelApp):
 
 # Add ipykernel's coroutines to cocotb's event loop
 def cocotb_loop_runner(coro: Coroutine[Any, Any, ExecutionResult]) -> ExecutionResult:
-    @cocotb.function
+    @cocotb.function # type: ignore
     async def coro_wrapper() -> ExecutionResult:
         return await coro
 
-    return coro_wrapper()
+    return coro_wrapper() # type: ignore
 
-@cocotb.test()
+@cocotb.test() # type: ignore
 async def kernel_entry(dut: SimHandleBase) -> None:
     app = CocotbKernelApp.instance(user_ns=dict(dut=dut))
 
-    def interrupt_kernel(_signal: signal.Signals, _frame: Any) -> None:
+    def interrupt_kernel(_signal: int, _frame: Any) -> None:
         # ipykernel<=6 uses a dedicated interrupt handler, however in this context the 
         # kernel is ran within a thread, preventing signals from being passed to the kernel.
         #
-        # TODO: This feature has been changed to a queue in a recent pull request
+        # TODO: ipykernel interrupts have been changed to use a queue in a recent pull request
         # https://github.com/ipython/ipykernel/pull/1079
         # Update this to call sigint_handler() on the next major update to ipykernel
         raise NotImplementedError('cocotb_kernel doesn\'t support interrupting execution')
     signal.signal(signal.SIGINT, interrupt_kernel)
 
     # Run ipykernel in a separate thread
-    @cocotb.external
+    @cocotb.external # type: ignore
     def start_kernel() -> None:
-        app.initialize(cocotb.argv)
-        app.shell.loop_runner = cocotb_loop_runner
-        app.start()
+        app.initialize(cocotb.argv) # type: ignore
+        app.shell.loop_runner = cocotb_loop_runner # type: ignore
+        app.start() # type: ignore
 
     cocotb.log.info('starting ipykernel')
     await start_kernel()
